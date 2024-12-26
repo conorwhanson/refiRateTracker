2be2ce48ebcf0b482155f5b9dce908bdc681ba0b
Conor Hanson <condorhanson@gmail.com>
2024-12-26 16:08:58 -0600

    OG Commit 12.26

---
+++ app.py
@@ -0,0 +1,56 @@
+import csv
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from datetime import date
+
+# build URl variables
+affRefiURL = "https://www.affinityplus.org/rates/mortgage-rates"
+rmRefiURL = "https://www.rocketmortgage.com/refinance-rates"
+blazeRefiURL = "https://blazecu.com/personal/borrow/mortgage/mortgage-refinance"
+magnifiRefiURL = "https://mymagnifi.org/rates/rates-mortgages.html"
+
+# Initialize Chrome webdriver
+chrome = webdriver.Chrome()
+
+#1 Get Affinity 30 year refi rate
+chrome.get(affRefiURL)
+time.sleep(3)
+rates = chrome.find_element(By.CLASS_NAME, "ratesTable")
+rates_list = rates.text.split("\n")
+refi_aff = rates_list[23:29]
+aff30refi = refi_aff[2].split(" ")[0]
+time.sleep(3)
+
+#2 Get Rocket Mortgage 30 year refi rate
+chrome.get(rmRefiURL)
+time.sleep(3)
+rkt_rates = chrome.find_elements(By.CLASS_NAME, "rkt-mv4")
+rktRefiRate = rkt_rates[0].text.split("\n")[1]
+time.sleep(3)
+
+#3 Get Blaze 30 year refi rate
+chrome.get(blazeRefiURL)
+time.sleep(3)
+blazeRates = chrome.find_element(By.CLASS_NAME, "rates-grid")
+blaze30Refi = blazeRates.text.split("\n")[-2:-1]
+blaze30Refi_rate = blaze30Refi[0].split(" ")[4]
+time.sleep(3)
+
+#4 Get Magnifi 30 year refi rate
+chrome.get(magnifiRefiURL)
+time.sleep(3)
+magnifi = chrome.find_element(By.CLASS_NAME, "table-responsive")
+magnifi30Refi = magnifi.text.split("\n")[4].split(" ")[-1:][0]
+time.sleep(3)
+
+# Get today's date
+rateDate = date.today().strftime("%m/%d/%Y")
+
+# Build list of data
+rates = [rateDate, aff30refi, rktRefiRate, blaze30Refi_rate, magnifi30Refi]
+
+# Write data to csv
+with open("refiRates.csv", "a", newline = '') as file:
+    writer = csv.writer(file, delimiter = ',')
+    writer.writerow(rates)
